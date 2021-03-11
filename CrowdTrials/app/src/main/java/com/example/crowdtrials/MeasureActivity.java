3c04d505ab653bedb277a19b6c74db87a1869328
Matthew Mullen <matthew.mullenx@gmail.com>
2021-03-11 12:47:17 -0700

    building some of these classes and corresponding interfaces for users when they try to add results/create results for a specific type of experiment

---
+++ CrowdTrials/app/src/main/java/com/example/crowdtrials/MeasureActivity.java
@@ -0,0 +1,7 @@
+package com.example.crowdtrials;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MeasureActivity extends AppCompatActivity {
+    // this will be the page that displays when adding results to an experiment/creating results
+}
---
+++ CrowdTrials/app/src/main/java/com/example/crowdtrials/NonNegativeCountActivity.java
@@ -0,0 +1,7 @@
+package com.example.crowdtrials;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NonNegativeCountActivity extends AppCompatActivity {
+    // this will be the page that displays when adding results to an experiment/creating results
+}
