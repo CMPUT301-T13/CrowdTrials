eedbba1b02f99f67e0fff259dc722f903d223a8a
Matthew Mullen <matthew.mullenx@gmail.com>
2021-04-02 23:29:21 -0600

    added a little more progress to it

--- CrowdTrials/app/src/main/java/com/example/crowdtrials/DetailActivity.java
+++ CrowdTrials/app/src/main/java/com/example/crowdtrials/DetailActivity.java
@@ -1,28 +1,35 @@
 package com.example.crowdtrials;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DetailActivity extends AppCompatActivity {
     Database db;
+    ListView reslist;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.displayresults);
+        setContentView(R.layout.listviewdetact);
+        Experiment exp = null;
         String type=(String) getIntent().getSerializableExtra("type");
         if(type.equals("meas")){
-            MeasurementExp exp = (MeasurementExp) getIntent().getSerializableExtra("exp");
+            exp = (MeasurementExp) getIntent().getSerializableExtra("exp");
         }
-        if(type.equals("bin")){
-            BinomialExp exp = (BinomialExp) getIntent().getSerializableExtra("exp");
+        else if(type.equals("bin")){
+            exp = (BinomialExp) getIntent().getSerializableExtra("exp");
         }
-        if(type.equals("ncount")){
-            NonNegativeCountExp exp = (NonNegativeCountExp) getIntent().getSerializableExtra("exp");
+        else if(type.equals("ncount")){
+            exp = (NonNegativeCountExp) getIntent().getSerializableExtra("exp");
         }
-        if(type.equals("count")){
-            CountExp exp = (CountExp) getIntent().getSerializableExtra("exp");
+        else if (type.equals("count")){
+            exp = (CountExp) getIntent().getSerializableExtra("exp");
         }
+        ArrayAdapter<ResultArr> resultAdapter = new ArrayAdapter<>(this,R.layout.displayresults,exp.results);
+        reslist.setAdapter(resultAdapter);
+
 
     }
 }
