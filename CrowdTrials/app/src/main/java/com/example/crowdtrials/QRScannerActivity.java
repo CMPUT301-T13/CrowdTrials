3914c0651832102eddb951f80a3e057b236b5ead
Sanjeev Kotha <kothasanju@gmail.com>
2021-04-08 04:37:31 -0600

    Added QR Commented Code and polished some misc code

---
+++ CrowdTrials/app/src/main/java/com/example/crowdtrials/QRScannerActivity.java
@@ -0,0 +1,14 @@
+package com.example.crowdtrials;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QRScannerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_q_r_scanner);
+    }
+}
