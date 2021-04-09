815624cd0850ae1aae02242bb8169a0e4c3234b9
Matthew Mullen <matthew.mullenx@gmail.com>
2021-04-09 14:06:25 -0600

    DO NOT RUN LAST INSTANCE 2 METHODS Break Database

--- CrowdTrials/app/src/main/java/com/example/crowdtrials/EditExperimentActivity.java
+++ CrowdTrials/app/src/main/java/com/example/crowdtrials/EditExperimentActivity.java
@@ -38,8 +38,8 @@ public class EditExperimentActivity extends AppCompatActivity {
         user = (User) getIntent().getSerializableExtra("user");
         exp = (Experiment) getIntent().getSerializableExtra("exp");
         if(exp.owner.username!=user.username){
-            //publish.setVisibility(View.GONE);
-            //ended.setVisibility(View.GONE);
+            publish.setVisibility(View.GONE);
+            ended.setVisibility(View.GONE);
         }
         tit.setText(exp.name);
         desc.setText(exp.description);
